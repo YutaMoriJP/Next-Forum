32f2b789fc776ed1a2f1a0ec3f59a54c0049b9df
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-09-16 16:22:12 -0700

    updated env var.

---
+++ .history/util/getAllPosts_20210916161938.ts
@@ -0,0 +1,9 @@
+export const getAllPosts = async () => {
+  const res = await fetch(
+    "http://localhost:8888/.netlify/functions/express/posts",
+    {
+      method: "GET",
+    }
+  ).then(res => res.json());
+  return res;
+};
