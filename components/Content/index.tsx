03367a8cdfa20c9556b3474ef3486f0ea102b202
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-09-18 12:38:58 -0700

    added server side rendering support for styledcomponent

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 import Box from "../../styles/Box";
 import Text from "../../styles/Text";
 import Title from "../../styles/Title";
-
 import BoxHeader from "../../styles/BoxHeader";
 import BoxContent from "../../styles/BoxContent";
 import Button from "../../styles/Button";
+//used to shorten title like 'BAYERN WINS AGAIN BY LARGE MARGIN' -> 'BAYERN WINS AGAIN...'
+import shortenText from "../../util/shortenText";
 
 interface ContentProps {
   title: string;
@@ -21,10 +22,12 @@ const Content = ({
   slug = "",
   main = false,
 }: ContentProps): JSX.Element => {
+  const shortenedTitle = shortenText(title, 5);
+  const shortenedReadMore = shortenText(title, 5);
   return (
     <Box>
       <BoxHeader>
-        <Title as="h1">{title}</Title>
+        <Title as="h3">{main ? shortenedTitle : title}</Title>
       </BoxHeader>
       <BoxContent>
         <Text weight={400}>{content}</Text>
@@ -32,7 +35,7 @@ const Content = ({
       {/* if main points at true, then <Content/> is rendered on the home page, if not, then it's the [slug].tsx page */}
       {main && (
         <Link href={`/${slug}`}>
-          <Button>{`READ ${title.toUpperCase()}`}</Button>
+          <Button>{`READ ${shortenedReadMore.toUpperCase()}`}</Button>
         </Link>
       )}
     </Box>
---
+++ util/shortenText.ts
@@ -0,0 +1,19 @@
+//sentence: 'GOLDEN STATE WARRIORS INTERESTED IN Gasol'
+//length: 5
+//@returns shorterSentence: ''GOLDEN STATE WARRIORS INTERESTED IN...'
+
+//sentence: 'Bayern wins again Barca'
+//length: 5
+//NO CHANGE, since sentence converted to an array has a smaller size than 5
+//@returns shorterSentence: 'Bayern wins again Barca'
+
+const shortenText = (sentence: string, length: number) => {
+  const sentenceArr: string[] = sentence.split(" ");
+  const shorterSentence: string =
+    sentence.length > length
+      ? sentenceArr.slice(0, length).join(" ") + "..."
+      : sentenceArr.join(" ");
+  return shorterSentence;
+};
+
+export default shortenText;
