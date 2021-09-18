405464b35d1ffe93fe331446148c19191e0107c3
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-09-17 18:02:29 -0700

    minor changes

--- .history/pages/_app_20210917175945.tsx
+++ .history/pages/_app_20210917175945.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+d;
 function MyApp({ Component, pageProps }: AppProps) {
   console.log("App", Component);
   console.log("pageprops", pageProps);
