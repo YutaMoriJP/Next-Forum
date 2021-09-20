5b7c47b020b52f186ef7ff9173622ea5d69ccb07
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-09-19 17:37:03 -0700

    tooltip updates

--- components/Icon/index.tsx
+++ components/Icon/index.tsx
@@ -10,15 +10,15 @@ const IconComponentWrapper = styled.article`
   margin: 0;
   .tooltiptext {
     visibility: hidden;
-    background-color: black;
+    background-color: #212529;
     color: #fff;
     text-align: center;
     border-radius: 6px;
-    padding: 6px;
+    padding: 5px;
     position: absolute;
     z-index: 1;
     font-size: 0.5rem;
-    width: max-content;
+    width: 100%;
     top: 100%;
     left: 50%;
     transform: translateX(-50%);
